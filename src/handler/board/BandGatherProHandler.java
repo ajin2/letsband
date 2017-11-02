06e73a199db9679669fb469cb0171ebc362c2cde
BOK\이원복 <wbookleee@naver.com>
2017-11-03 03:20:20 +0900

    test2

--- src/handler/board/BandGatherProHandler.java
+++ src/handler/board/BandGatherProHandler.java
@@ -6,9 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import handler.CommandHandler;
 
 public class BandGatherProHandler implements CommandHandler{
-
+      
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		int a;
 		return "/board/gatherPro.jsp";
 	}
 }
